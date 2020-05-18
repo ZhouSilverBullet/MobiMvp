863b5ffea8960652bd44fe74920dbe410bfcd399
zhousaito <zhousaito@163.com>
2020-05-18 23:44:40 +0800

    沉浸式

--- mobi_mvp/src/main/java/com/mobi/base/BaseActivity.java
+++ mobi_mvp/src/main/java/com/mobi/base/BaseActivity.java
@@ -1,11 +1,15 @@
 package com.mobi.base;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.mobi.dialog.LoadingDialog;
+import com.mobi.util.StatusBarPaddingUtil;
 import com.trello.rxlifecycle2.components.support.RxAppCompatActivity;
 
 import butterknife.ButterKnife;
@@ -25,7 +29,15 @@ public abstract class BaseActivity extends RxAppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (isFitWindow()) {
+            fitWindow();
+        }
+
         setContentView(getLayoutId());
+
+        //设置状态栏的颜色
+        setDarkStatusIcon(false);
 
         bind = ButterKnife.bind(this);
 
@@ -56,6 +68,7 @@ public abstract class BaseActivity extends RxAppCompatActivity {
 
     /**
      * 处理传过来的intent数据
+     *
      * @param intent
      */
     protected void initVariables(@NonNull Intent intent) {
@@ -68,6 +81,7 @@ public abstract class BaseActivity extends RxAppCompatActivity {
 
     /**
      * 一般的用不上
+     *
      * @param savedInstanceState
      */
     protected void initView(Bundle savedInstanceState) {
@@ -89,6 +103,29 @@ public abstract class BaseActivity extends RxAppCompatActivity {
             bind.unbind();
         }
         bind = null;
+    }
+
+    /**
+     * 默认不打开
+     *
+     * @return
+     */
+    protected boolean isFitWindow() {
+        return false;
+    }
+
+    private void fitWindow() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window window = getWindow();
+            // Translucent status bar
+            window.setFlags(
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+    }
+
+    public void setDarkStatusIcon(boolean bDark) {
+        StatusBarPaddingUtil.statusBar(this, bDark);
     }
 
     protected void showProgressDialog() {
